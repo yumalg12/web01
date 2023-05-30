843d8f4fc5497728248f11fdb8b86651906ab640
Malg Eum Yu <yumalg12@gmail.com>
2023-05-31 01:07:02 +0900

    Search 업데이트

--- script/search.js
+++ script/search.js
@@ -1,10 +1,18 @@
 
-function submitForm() {
-	var input = document.getElementById("naverMap");
-	var searchText = input.value;
-
-	var url = "https://map.naver.com/v5/search/" + searchText;
-	
+function navMapForm() {
+	var input = document.getElementById("naverMap").value;
+	var url = "https://map.naver.com/v5/search/" + input;
 	window.open(url);
 }
 
+function GoogleKoEn() {
+	var input = document.getElementById("GoogleKoEn").value;
+	var url = "https://translate.google.com/?hl=ko&sl=en&tl=ko&text=" + input + "&op=translate";
+	window.open(url);
+}
+
+function GoogleEnKo() {
+	var input = document.getElementById("GoogleEnKo").value;
+	var url = "https://translate.google.com/?sl=en&tl=ko&text=" + input + "&op=translate";
+	window.open(url);
+}
